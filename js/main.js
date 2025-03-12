c38321c44eccf81c501f48620cd8aa58261e7071
Celia Kelly <celiackelly@gmail.com>
2025-03-12 11:45:53 -0400

    Home page nav and header

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Doesn't works because of when this script executes vs. when the screen is painted.
+// Might need to hard code it on each page, or add a click event listener
+//https://css-tricks.com/snippets/jquery/highlight-all-links-to-current-page/ 
+
+// const navLinks = document.querySelectorAll('.nav-link-container a')
+// navLinks.forEach(link => {
+//     if (link.getAttribute("href") == window.location.pathname) {
+//         link.classList.add("current")
+//     }
+//     else {
+//         link.classList.remove("current")
+//     }
+// })
+
+
